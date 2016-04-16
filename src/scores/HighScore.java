716749ca0e52fdf7eb180bd1b129096a0d57127e
Darkyler <darkyler57@gmail.com>
2016-04-17 01:19:15 +0200

    TestHighScore and most of HighScore (missing JSON parsing

--- src/scores/HighScore.java
+++ src/scores/HighScore.java
@@ -1,5 +1,13 @@
 package scores;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
 /**
 *   HighScore class interacting with distant data
 *   @author: AFONSO Benjamin and CABOURG Max
@@ -13,8 +21,18 @@ public class HighScore {
   */
   public String[] getScores(){
 
+    String res = "";
+    // Starting a connexion to the API
+    HttpURLConnection conn = GET("https://api.thingspeak.com/channels/108875/feeds.json?results=2");
+    // Reading the distant ressource
+    res = readBuffer(conn);
+    // Printing the JSON.
+    System.out.println(res);
+
     return null;
   }
+
+
 
   /**
   * Simulate a player playing.
@@ -30,6 +48,39 @@ public class HighScore {
   */
   public void sendScore(String[] score){
 
+  }
+
+  // Read the buffer contained in the given HttpURLConnection
+  private String readBuffer(HttpURLConnection conn){
+    String output = null;
+    String res = "";
+    try {
+
+      BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+      while ((output = br.readLine()) != null){
+        res = res+output;
+      }
+      conn.disconnect();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+      return res;
+
+  }
+
+  // Open a HttpURLConnection to read the result.
+  private HttpURLConnection GET(String target){
+
+    HttpURLConnection conn = null;
+    try {
+      URL url = new URL(target);
+      conn = (HttpURLConnection) url.openConnection();
+      conn.setRequestMethod("GET");
+      conn.setRequestProperty("Accept", "application/json");
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+      return conn;
   }
 
 }
