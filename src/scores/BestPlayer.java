498a839c3dcf6f176ba19d2806615c3497800deb
Darkyler <Darkyler57@gmail.com>
2016-04-16 15:56:01 +0200

    Adding some squeletons

---
+++ src/scores/BestPlayer.java
@@ -0,0 +1,11 @@
+package scores;
+
+public class BestPlayer{
+
+  public int compareTo(BestPlayer p){
+
+    return 0;
+  }
+
+  // tenBestScores, ...
+}
--- src/scores/HighScore.java
+++ src/scores/HighScore.java
@@ -1,11 +1,35 @@
 package scores;
 
 /**
-*
-*
-*
+*   HighScore class interacting with distant data
+*   @author: AFONSO Benjamin and CABOURG Max
+*   @see: scores#TestHighSCore
 */
 public class HighScore {
 
+  /*
+  *  Calls the API server and returns the scores
+  *  @return: String[]
+  */
+  public String[] getScores(){
+
+    return null;
+  }
+
+  /**
+  * Simulate a player playing.
+  *
+  */
+  public void play(){
+
+  }
+
+  /**
+  *  Send score to the distant API Server
+  *
+  */
+  public void sendScore(String[] score){
+
+  }
 
 }
